2e986d1e3f433f602117b04c300950da3e24dcd6
Yashashav Goyal <krishnaSaxena271@gmail.com>
2023-08-05 19:15:09 +0530

    landing page Animation Done

--- js/script.js
+++ js/script.js
@@ -4,6 +4,31 @@ const scroll = new LocomotiveScroll({
 });
 // el means element top level element i.e. main in our website
 
+function firstPageAnime() {
+    let tl = gsap.timeline();
+
+    tl.from('nav', {
+        y:'-10',
+        opacity:0,
+        duration:1.5,
+        ease: Expo.easeInOut
+    })
+    .to('.boundingElem', {
+        y:0,
+        ease:Expo.easeInOut,
+        duration:2,
+        delay:-1,
+        stagger:.2
+    })
+    .from('#heroFooter', {
+        y:-10,
+        opacity:0,
+        duration:1.5,
+        delay:-1,
+        ease:Expo.easeInOut
+    })
+}
+
 function circleMouseFollower() {
     window.addEventListener("mousemove", function(details) {
         // console.log(details);  
@@ -13,3 +38,4 @@ function circleMouseFollower() {
 }
 
 circleMouseFollower();
+firstPageAnime();
