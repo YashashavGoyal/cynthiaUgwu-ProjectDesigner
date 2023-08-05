e7821b5cfd451a52ff8400c526b3a9ef4fcb22b5
Yashashav Goyal <krishnaSaxena271@gmail.com>
2023-08-05 18:34:05 +0530

    mouseCircle Done

--- js/script.js
+++ js/script.js
@@ -2,5 +2,14 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
-
 // el means element top level element i.e. main in our website
+
+function circleMouseFollower() {
+    window.addEventListener("mousemove", function(details) {
+        // console.log(details);  
+        // console.log(details.clientX, details.clientY);  
+        this.document.querySelector(`#mouseCircle`).style.transform = `translate(${details.clientX}px, ${details.clientY}px)`;
+    })
+}
+
+circleMouseFollower();
