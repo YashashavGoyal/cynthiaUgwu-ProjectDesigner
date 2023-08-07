8049d41b31941e7880ac842b9bf5458c2f108764
Yashashav Goyal <krishnaSaxena271@gmail.com>
2023-08-07 12:54:22 +0530

    applying photo moving functionality

--- js/script.js
+++ js/script.js
@@ -69,3 +69,44 @@ function circleMouseFollower(xScale, yScale) {
 circleShapeChanger();
 circleMouseFollower();
 firstPageAnime();
+
+document.querySelectorAll(`.elem`).forEach(function (elem) {
+    var rotate = 0;
+    var diffrot = 0;
+  
+    elem.addEventListener(`mouseleave`, function (dets) {
+      gsap.to(elem.querySelector(`img`), {
+        opacity: 0,
+        ease: Power3,
+        duration: 0.5,
+      });
+    });
+  
+    elem.addEventListener(`mousemove`, function (dets) {
+      var diff = dets.clientY - elem.getBoundingClientRect().top;
+      diffrot = dets.clientX - rotate;
+      rotate = dets.clientX;
+      gsap.to(elem.querySelector(`img`), {
+        opacity: 1,
+        ease: Power3,
+        top: diff,
+        left: dets.clientX,
+        rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+      });
+    });
+});
+  
+
+document.querySelectorAll(`.footerItem`).forEach(function(elem) {
+    let icon = elem.querySelector(`i`)
+
+    elem.addEventListener(`mousemove`, function(details) {
+        icon.classList.remove(`ri-arrow-right-up-line`);
+        icon.classList.add(`ri-arrow-right-line`);
+    })  
+    
+    elem.addEventListener(`mouseleave`, function(details) {
+        icon.classList.remove(`ri-arrow-right-line`);
+        icon.classList.add(`ri-arrow-right-up-line`);
+    })
+})
